9d5d1d932e1cece6c0093d462c8de21719752428
Shashwat Awasthi <53440849+ShashwatAwasthi04@users.noreply.github.com>
2020-05-20 01:29:15 +0530

    Added router

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ const http = require('http');
 const PORT =process.env.port || 5000;
 
 const app = express();
+const router = require('./router');
 
 const server = http.createServer(app);
 const io = socketio(server);
+
+
+io.on('connection', (socket) => {
+    console.log('New Connection');
+
+    socket.on('disconnect', () => {
+     console.log('User left');   
+    })
+});
+
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`server started ${PORT}`));
