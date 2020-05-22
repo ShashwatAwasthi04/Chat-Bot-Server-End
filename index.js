6c32d38af6d605aca82a8f269abe810ff152066b
Shashwat Awasthi <53440849+ShashwatAwasthi04@users.noreply.github.com>
2020-05-23 02:01:34 +0530

    Resolved issues arising of Undefined properties

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ io.on('connection', (socket) => {
     socket.on('sendMessage', (message) => {
         const user = getUser(socket.id);
 
-        io.to(user.room.emit('message', {user:user.name, text:message}));
+        io.to(user.room).emit('message', { user: user.name, text: message });
 
         callback();
     });
