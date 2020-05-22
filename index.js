9aed19ddcd57bcf84c8659078fcacebad74fde50
Shashwat Awasthi <53440849+ShashwatAwasthi04@users.noreply.github.com>
2020-05-23 02:09:59 +0530

    Added the user connection admin message

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ io.on('connection', (socket) => {
 
         socket.join(user.room);
         
-
+        io.to(user.room).emit('roomData', {room:user.room,users:getUserRoom(user.room)})
         callback();
     });
 
@@ -34,12 +34,19 @@ io.on('connection', (socket) => {
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+
 
         callback();
     });
 
     socket.on('disconnect', () => {
-     console.log('User left');   
+      const user = removeUser(socket.id);
+      
+      if(user) {
+        io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+        
+      }
     })
 });
 
